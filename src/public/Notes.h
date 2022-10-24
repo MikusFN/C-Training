7c4d844624e69c0ad39fc34840f67a2700e8b550
fernando.camilo <fernando.camilo@fabamaq.com>
2022-10-24 23:56:05 +0100

    Notes.h file with vars and headers notes (8 and 10)

---
+++ src/public/Notes.h
@@ -0,0 +1,24 @@
+/** Notes for vars
+ * char -> 1 byte;
+ * bool -> 1 byte; (could be a bit but we can only acess bytes not bits)
+ * short -> 2 byte;
+ * int -> 4 byte;
+ * float -> 4 byte (needs a f attached to be a true float);
+ * double -> 8 byte;
+ * long -> 4 byte (Depending on the compiler);
+ * long long-> 8 byte (Depending on the compiler);
+ * Can be determined by sizeof
+ * 
+*/
+
+/** Notes for headers
+ * #pragma once -> include guard to only include the header once in a cpp file
+ * #ifndef then #define (included code) #endif
+ * <> include form one of the include directories (everything)
+ * "" compiler include paths
+ * Files without .h extension are from the c++ standard lib (with .h are from the c standard lib)
+*/
+
+/** Notes for 
+ * 
+*/
